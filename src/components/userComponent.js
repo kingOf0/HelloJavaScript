0bb7abc4d71eb635fb571ed899cbca253a7251e2
kingOf0 <ibrahimbugra1077@gmail.com>
2021-11-25 00:07:21 +0300

    removed some commits

--- src/components/userComponent.js
+++ src/components/userComponent.js
@@ -7,28 +7,11 @@ console.log("User component yüklendi")
 
 let logger1 = new MongoLogger()
 let userService = new UserService(logger1)
-
-let user1 = new User(1,"Engin","Demiroğ","Ankara")
-let user2 = new User(2,"Baran","Gökçekli","Muğla")
-userService.addUser(user1)
-userService.addUser(user2)
-
-//console.log(userService.list())
-//console.log(userService.getById(2))
-
-
-
-
-let customer = {id:1, firstName:"Engin"}
-
-//prototyping
-customer.lastName = "Demiroğ"
-
-console.log(customer.lastName)
-
-console.log("--------------------------")
+userService.addUser(new User(1,"Engin","Demiroğ","Ankara"))
+userService.addUser(new User(2,"Baran","Gökçekli","Muğla"))
 userService.load()
 
+console.log("--------------------------")
 
 let lolCustomer = new Customer(1, "Seda", "Yılmaz", "Ankara", "fdgdfg");
 lolCustomer.type = "customer"
@@ -38,4 +21,3 @@ console.log(userService.getCustomers())
 console.log(userService.getEmployees())
 console.log(userService.errors)
 console.log(userService.getCustomersSorted())
-//22.00 Dersteyiz
